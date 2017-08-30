ce6275bac99092211ab5100b8130f87275f77e07
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-08-30 17:53:20 -0300

    Variavel referencia para todos os comandos teste

--- src/ecoflow/ModbusMaster.java
+++ src/ecoflow/ModbusMaster.java
@@ -13,11 +13,12 @@ public class ModbusMaster {
     public static void main(String[] args) throws Exception {
         
         //Variavel de leitura e escrita
-        int count = 5; //Varialvel contador
+        int count = 8; //Varialvel contador
+        int ref = 8; //Rerencia de inicio
         int[] valores = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}; //Escrever Multiplos registros
         int[] respostas = new int[count]; // Leitura multiplos registros
         BitVector bitVector = new BitVector(count);//Leitura Coils Outputs
-        byte[] byteVector = {11}; //Numeros para BitVector
+        byte[] byteVector = {1}; //Numeros para BitVector
         BitVector bitVectorValor = new BitVector( byteVector.length );//Inicializando BitVector
         bitVectorValor = bitVectorValor.createBitVector( byteVector );//Transforma em binarios de 8bit
         
@@ -31,35 +32,35 @@ public class ModbusMaster {
         conexao = Conexao.Connection("127.0.0.1", Modbus.DEFAULT_PORT);
         
         //Escrever no registro unica vez
-        //System.out.println( registros.escreverUnicoRegistro(conexao, 1, 250) );
+        //System.out.println( registros.escreverUnicoRegistro(conexao, ref, 250) );
         
         //Escrever multiplos registros
-        //System.out.println( registros.escreverMultiplosRegistros(conexao, 0, valores) );
+        //System.out.println( registros.escreverMultiplosRegistros(conexao, ref, valores) );
                 
         //Ler os registros
-        /*respostas = registros.lerMultiplosRegistros(conexao, 7, count);
+        /*respostas = registros.lerMultiplosRegistros(conexao, ref, count);
         for(int i = 0; i < count; i++){
             System.out.println( respostas[i] );
         }*/
         
         //Ler os entrada registro
-        /*respostas = entradaRegistro.lerEntradaRegistros(conexao, 5, count);
+        /*respostas = entradaRegistro.lerEntradaRegistros(conexao, ref, count);
         for(int i = 0; i < count; i++){
             System.out.println( respostas[i] );
         }*/
         
         //Escrever boelano
-        //System.out.println( boleano.escreverBoleano(conexao, 4, false) );
+        //System.out.println( boleano.escreverBoleano(conexao, ref, false) );
         
         //Escrever multiplos boleanos
-        System.out.println( boleano.escreverMultiplosBoleanos(conexao, 0, bitVectorValor) );
+        System.out.println( boleano.escreverMultiplosBoleanos(conexao, ref, bitVectorValor) );
         System.out.println( bitVectorValor.toString() );
         
         //Ler boleanos
-        /*bitVector = boleano.lerBoleano(conexao, 11, count);
+        bitVector = boleano.lerBoleano(conexao, ref, count);
         for(int i = 0; i < count; i++){
             System.out.println(bitVector.getBit(i) );
-        }*/
+        }
         
         //Fechar conexão
         conexao.close();
